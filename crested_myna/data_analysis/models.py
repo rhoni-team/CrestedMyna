b7352c9d77995dd73abe6cfd996e4675103cd782
nataliamd11 <nataliamdenk11@gmail.com>
2024-10-18 18:34:29 -0300

    feat: modify model to add new ebird fields

--- crested_myna/data_analysis/models.py
+++ crested_myna/data_analysis/models.py
@@ -5,26 +5,24 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 
-def get_year(instance):
-    """Get the year from the observation date."""
-    return instance.observation_date.year
-
-
 class ACRecord(models.Model):
     """
     Model for the AC records.
     """
-    observation_count = models.IntegerField(null=True)
-    country = models.CharField(max_length=150)
-    country_code = models.CharField(max_length=2)
-    state = models.CharField(max_length=150)
-    state_code = models.CharField(max_length=150)
+    checklist_id = models.CharField(max_length=100)
     latitude = models.FloatField(validators=[MinValueValidator(-90), 
                                              MaxValueValidator(90)])
     longitude = models.FloatField(validators=[MinValueValidator(-180), 
                                               MaxValueValidator(180)])
+    country_code = models.CharField(max_length=2)
+    country = models.CharField(max_length=150)
+    state = models.CharField(max_length=150)
+    state_code = models.CharField(max_length=150)
     observation_date = models.DateField(null=False)
-    year = models.IntegerField(default=get_year)
+    year = models.IntegerField(null=True)
+    observation_count = models.IntegerField(null=True, blank=True)
+    time_observations_started = models.TimeField(null=True)
+    duration_minutes = models.IntegerField(null=True)
 
     class Meta:
         """Meta options for the ACRecord model."""
