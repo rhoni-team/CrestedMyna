14cc6790b24ce3b23ebe7dd6759f68a1849780c5
nataliamd11 <nataliamdenk11@gmail.com>
2024-10-21 16:18:58 -0300

    feat: get database context

--- crested_myna/mapping/views.py
+++ crested_myna/mapping/views.py
@@ -1,11 +1,15 @@
 """Main View to load Vue app in Django html template"""
 from django.views.generic.base import TemplateView
+from data_analysis.models import ACRecord
 
 
 class IndexTemplateView(TemplateView):
     """Loads index.html where the frontend is set
     """
 
-    def get_template_names(self):
-        template_name = 'mapping/home.html'
-        return template_name
+    template_name = 'mapping/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['locations'] = ACRecord.objects.all()[:5]
+        return context
