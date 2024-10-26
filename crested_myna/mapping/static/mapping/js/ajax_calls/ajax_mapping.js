0af0d2225cd9a623155b85e0c295ac37a420a46c
nataliamd11 <nataliamdenk11@gmail.com>
2024-10-26 08:30:51 -0300

    feat: ajax call to get all records

---
+++ crested_myna/mapping/static/mapping/js/ajax_calls/ajax_mapping.js
@@ -0,0 +1,19 @@
+// mapping/js/ajax_calls/ajax_mapping.js
+
+export function getAllPoints() {
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            url: '/get-records/',
+            type: "GET",
+            dataType: "json",
+            success: (data) => {
+                console.log("records in ajax", data.records);
+                resolve(data.records);  // Resolve the Promise with the data
+            },
+            error: (error) => {
+                console.log(error);
+                reject(error);  // Reject the Promise with the error
+            }
+        });
+    });
+}
