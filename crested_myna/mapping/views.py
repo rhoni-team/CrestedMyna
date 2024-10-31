1e5a72f5af05e19e0e56bd428c669023413e01a0
nataliamd11 <nataliamdenk11@gmail.com>
2024-10-31 14:55:13 -0300

    feat: add GetCountriesPolygons view

--- crested_myna/mapping/views.py
+++ crested_myna/mapping/views.py
@@ -1,8 +1,13 @@
 """Main View to load Vue app in Django html template"""
+from typing import List
+
 from django.http import JsonResponse, HttpResponseBadRequest
 from django.views import View
 from django.views.generic.base import TemplateView
+from django.core.serializers import serialize
+
 from data_loading.models import ACRecord
+from data_loading.models import WorldBorder
 
 
 class IndexTemplateView(TemplateView):
@@ -27,8 +32,35 @@ class GetRecords(View):
         if is_ajax:
             records = ACRecord.objects.values_list('location', flat=True)
             response = JsonResponse({ "records": list(records)})
-            print(response)
             return response
         
         else:
             return HttpResponseBadRequest('Invalid request')
+
+
+class GetCountriesPolygons(View):
+    """Get countries polygons from the database"""
+
+    def get_country_code_for_countries_with_ac_records(self) -> List["str"]:
+        """Get a list of country codes for countries with AC records"""
+        all_records = ACRecord.objects.all().order_by('country_code')
+        country_codes = all_records.distinct('country_code').values_list('country_code', flat=True)
+        return country_codes
+    
+    def get_countries_polygons_with_ac_records(self, country_codes: List["str"]) -> List["WorldBorder"]:
+        """Get a list of countries polygons for countries with AC records"""
+        countries_pol = WorldBorder.objects.filter(iso2__in=country_codes)
+        return countries_pol
+    
+    def serialize_countries_polygons_as_geojson(self, countries_polygons: List["WorldBorder"]) -> str:
+        """Serialize countries polygons as geojson"""
+        geojson = serialize('geojson', countries_polygons)
+        return geojson
+
+    def get(self, request):
+        """Get the countries polygons with AC records as geojson"""
+        countries = self.get_country_code_for_countries_with_ac_records()
+        countries_polygons = self.get_countries_polygons_with_ac_records(countries)
+        geojson_pol = self.serialize_countries_polygons_as_geojson(countries_polygons)
+        return JsonResponse({ "countries": geojson_pol})
+    
