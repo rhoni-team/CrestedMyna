b752fe1287810e54d1b0a97bfd293ed0d776c958
nataliamd11 <nataliamdenk11@gmail.com>
2024-11-08 17:02:07 -0300

    feat: add cities model

--- crested_myna/data_loading/models.py
+++ crested_myna/data_loading/models.py
@@ -84,6 +84,18 @@ class CitiesEsri(models.Model):
     def __str__(self):
         """String representation of the Country model."""
         return self.city_name if self.city_name else "Unnamed City"
+    
+class Cities(models.Model):
+    """
+    Model for the cities.
+    """
+    city_name = models.CharField(max_length=250, null=True)
+    population = models.IntegerField(null=True)
+    lat = models.FloatField(validators=[MinValueValidator(-90),
+                                             MaxValueValidator(90)], null=True)
+    lon = models.FloatField(validators=[MinValueValidator(-180),
+                                              MaxValueValidator(180)], null=True)
+    geom = models.PointField(null=True, srid=3857)
 
 
 class CountrySimplified(models.Model):
